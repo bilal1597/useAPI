09ec720ebd19fcab5b964a58af079eab78dc17ab
Bilal Ilyas <bilalilyas2597@gmail.com>
2024-11-22 17:43:30 +0500

    cru API

--- app/Http/Controllers/API/PostController.php
+++ app/Http/Controllers/API/PostController.php
@@ -7,6 +7,8 @@ use App\Models\Post;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 
+use function PHPUnit\Framework\fileExists;
+
 class PostController extends Controller
 {
     /**
@@ -14,7 +16,7 @@ class PostController extends Controller
      */
     public function index()
     {
-        $data['post'] = Post::all();
+        $data['posts'] = Post::all();
         return response()->json([
             'status' => true,
             'message' => 'All Posts',
@@ -45,11 +47,16 @@ class PostController extends Controller
             ], 401);
         }
 
+        $img = $request->image;
+        // $ext = $img->getClientOriginalExtension();
+        $ext = $img->extension();
+        $imageName = time() . '.' . $ext;
+        $img->move(public_path() . '/uploads/posts', $imageName);
 
         $post = Post::create([
             'title' => $request->title,
             'description' => $request->description,
-            'image' => $request->image
+            'image' => $request->$imageName
         ]);
 
         return response()->json([
@@ -64,7 +71,17 @@ class PostController extends Controller
      */
     public function show(string $id)
     {
-        //
+        $data['post'] = Post::select(
+            'id',
+            'title',
+            'description',
+            'image',
+        )->where(['id' => $id])->get();
+        return response()->json([
+            'status' => true,
+            'message' => 'Single Post',
+            'data' => $data,
+        ], 200);
     }
 
     /**
@@ -72,7 +89,55 @@ class PostController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        //
+        $userValidate = Validator::make(
+            $request->all(),
+            [
+                'title' => 'required',
+                'description' => 'required',
+                'image' => 'required|mimes:png,jpg,jpeg,webp,gif'
+            ]
+
+        );
+
+        if ($userValidate->fails()) {
+            return response()->json([
+                'status' => false,
+                'message' => 'Validation Error',
+                'errors' => $userValidate->errors()->all()
+            ], 401);
+        }
+
+        $post = Post::select('id', 'image')->get();
+        if ($request->image != '') {
+            $path = public_path() . '/uploads/posts';
+            if ($post->image != '' && $post->image != null) {
+                $oldFile = $path . $post->image;
+                if (fileExists($oldFile)) {
+                    unlink($oldFile);
+                }
+            }
+
+            $img = $request->image;
+            // $ext = $img->getClientOriginalExtension();
+            $ext = $img->extension();
+            $imageName = time() . '.' . $ext;
+            $img->move(public_path() . '/uploads/posts', $imageName);
+        } else {
+            $imageName = $post->image;
+        }
+
+
+        $post = Post::where(['id' => $id])->update([
+            'title' => $request->title,
+            'description' => $request->description,
+            'image' => $request->$imageName
+        ]);
+
+        return response()->json([
+            'status' => true,
+            'message' => 'Post Updated Successfully',
+            'post' => $post,
+        ], 200);
     }
 
     /**
